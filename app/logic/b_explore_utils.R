c204f359674207b87aff6fa8d23ae5a3ec396187
RVel37 <rvelissaris@gmail.com>
2023-04-20 09:59:13 +0100

    explore page

--- app/logic/b_explore_utils.R
+++ app/logic/b_explore_utils.R
@@ -1,9 +1,30 @@
 box::use(
-  shinipsum[...]
+  shinipsum[random_ggplot],
+  shiny[...],
+  prompter[add_prompt]
 )
 
-
+# ------------------------ plotting
 #'@export
 get_random_ggplot <- function(plot_type) {
   random_ggplot(plot_type)
+}
+
+# ------------------------ ? button
+#'@export
+helpButton <- function(message) {
+  return(
+    add_prompt(
+      ui_element = span(HTML('<i class="fa fa-question-circle"></i>')),
+      message = message, position = "right"
+    )
+  )
+}
+
+# ------------------------ Make headers
+#'@export
+makeHeaders <- function(title, message, fs=1.3) {
+  tagList(
+    span(span(title, style=paste0("font-size: ", fs, "em;")), helpButton(message))
+  )
 }
--- app/view/b_explore.R
+++ app/view/b_explore.R
@@ -6,7 +6,9 @@ box::use(
   shinipsum[...],
 )
 box::use(
-  app/logic/b_explore_utils[get_random_ggplot],
+  app/logic/b_explore_utils[get_random_ggplot,
+                            helpButton,
+                            makeHeaders],
 )
 
 #'@export
@@ -34,7 +36,7 @@ ui <- function(id) {
             fluidRow(
               column(
                 6,
-# GSE drop-down
+# GSE drop-down -----------------------------
                 selectInput(
                   inputId = ns("selectStudy"),
                   label = "Study",
@@ -43,7 +45,7 @@ ui <- function(id) {
 
               ),  #here will go the uioutput (study link)
 
-# study contrasts drop-down
+# study contrasts drop-down ------------------
               column(
                 width = 6,
                 selectInput(
@@ -59,7 +61,7 @@ ui <- function(id) {
               column(
                 width = 12,
 
-# Results table
+# Results table ----------------------------
                 h3("Results Table"),
                 p("IBD RNA-Seq DEG analysis results."),
                 withSpinner(DTOutput(ns("degTable")))
@@ -75,19 +77,157 @@ ui <- function(id) {
           column(
             width = 12,
             tabsetPanel(
+# Expression plot -------------------------
               id = "expTabset",
               tabPanel(
                 title = "Expression",
                 icon=icon('chart-bar'),
                 selectInput(
-                  inputId = ns("selectNorm"),
+                  inputId = ns("selectNormExp"),
                   label = "Normalization",
                   selected = "CPM",
                   choices = c("CPM", "TPM", "RPKM")
                 ),
-                uiOutput(ns("html")),
+                makeHeaders(ns(
+                  title = "Gene counts ",
+                  message=paste0("Gene count plots for samples in the selected study.")
+                )),
+                hr(),
+                uiOutput(ns("expHtml")),
                 plotOutput(ns("expPlot"))
               ),
+# Volcano plot----------------------------
+              tabPanel(
+                title = "Volcano plot",
+                icon=icon('mountain'),
+                fluidRow(
+                  column(
+                    width = 6,
+                    hr(),
+                    makeHeaders(
+                      title = "Volcano plot ",
+                      message=paste0("Volcano plot showing the differential gene expression results.")
+                    ),
+                    hr()
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    uiOutput(ns("volcanoHtml")),
+                    plotOutput(ns("volcanoPlot"))
+                  )
+                )
+              ),
+# Heatmap ------------------------------
+              tabPanel(
+                title = "Heatmap",
+                icon=icon("burn"),
+                #Heatmap_panel()
+                fluidRow(
+                  column(
+                    width = 6,
+                    hr(),
+                    makeHeaders(
+                      title = "Heatmap ",
+                      message = paste0("Heatmap of top DEG count plots for samples in the selected study.")
+                    ),
+                    hr()
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    selectInput(
+                      inputId = ns("selectNormHeatmap"),
+                      label = "Normalization",
+                      selected = "CPM",
+                      choices = c("CPM", "TPM", "RPKM")
+                    ),
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    uiOutput(ns("heatmapHtml")),
+                    plotOutput(ns("heatmap"))
+
+                  )
+                ),
+              br(),
+              ),
+# Pathway analysis ------------------------------
+              tabPanel(
+                title = "Pathway analysis",
+                icon=icon("project-diagram"),
+                #Enrich_panel()
+                fluidRow(
+                  column(
+                    width = 6,
+                    hr(),
+                    makeHeaders(
+                      title = "KEGG enrichment ",
+                      message=paste0("Heatmap of top hits from KEGG pathway enrichment (via 'enrichr' web service) in over- and under-expressed genes.")
+                    ),
+                    hr()
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    selectInput(
+                      inputId = "selectEM",
+                      choices = c("Combined.Score", "Odds.Ratio", "Padj (-log10)"),
+                      selected = "Combined.Score",
+                      label = "Enrichment Metric"
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    uiOutput(ns("enrichHtml")),
+                    plotOutput(ns("enrichPlot"))
+                    )
+                  )
+                ),
+                br(),
+              ),
+# Comparison ------------------------------
+              tabPanel(
+                title = "Comparison",
+                icon=icon("adjust"),
+                #Upset_panel()
+                fluidRow(
+                  column(
+                    width = 6,
+                    hr(),
+                    makeHeaders(
+                      title = "DEG comparison ",
+                      message=paste0("UpSet plot comparing over- and under-expressed genes between studies.")
+                    ),
+                    hr()
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    selectInput(
+                      inputId = "upsetSelect",
+                      choices = c("Over-expressed", "Under-expressed"),
+                      selected = "Over-expressed",
+                      label = "DEG type"
+                    )
+                  )
+                ),
+                fluidRow(
+                  column(
+                    width = 12,
+                    uiOutput(ns("upsetHtml")),
+                    plotOutput(ns("upsetPlot"))
+                  )
+                ),
+                br()
+              )
             ))
         ))))
 }
@@ -105,18 +245,75 @@ server <- function(id) {
       random_DT(10,3)
     })
 
-    output$volcano_plot <- renderPlot({
-      random_ggplot()
-      # create_exp_plot(input$selectStudy, input$selectContrast, input$selectNorm)
-    })
+
+#-------------------------------------------------------
+# plots for each tab
+#-------------------------------------------------------
+
+#----------------------- EXPRESSION PLOT
 
     output$expPlot <- renderPlot({
       get_random_ggplot(input$selectStudy)
     })
 
-    output$html <- renderUI({
+# NOTE -- can later be converted to:
+# create_exp_plot(input$selectStudy, input$selectContrast, input$selectNorm)
+
+    output$expHtml <- renderUI({
       string <- paste0(
-        input$selectStudy, input$selectContrast, input$selectNorm
+        input$selectStudy, input$selectContrast, input$selectNormExp
+      )
+      string
+    })
+
+#----------------------- VOLCANO PLOT
+
+    output$volcanoPlot <- renderPlot({
+      get_random_ggplot(input$selectStudy)
+    })
+
+    output$volcanoHtml <- renderUI({
+      string <- paste0(
+        input$selectStudy, input$selectContrast
+      )
+      string
+    })
+
+#----------------------- HEATMAP
+
+    output$heatmap <- renderPlot({
+      get_random_ggplot(input$selectStudy)
+    })
+
+    output$heatmapHtml <- renderUI({
+      string <- paste0(
+        input$selectStudy, input$selectContrast, input$selectNormHeatmap
+      )
+      string
+    })
+
+#----------------------- PATHWAY ANALYSIS
+
+    output$enrichPlot <- renderPlot({
+      get_random_ggplot(input$selectStudy)
+    })
+
+    output$heatmapHtml <- renderUI({
+      string <- paste0(
+        input$selectStudy, input$selectContrast, input$selectEM
+      )
+      string
+    })
+
+#----------------------- COMPARISON
+
+    output$upsetPlot <- renderPlot({
+      get_random_ggplot(input$selectStudy)
+    })
+
+    output$upsetHtml <- renderUI({
+      string <- paste0(
+        input$selectStudy, input$selectContrast, input$upsetSelect
       )
       string
     })
