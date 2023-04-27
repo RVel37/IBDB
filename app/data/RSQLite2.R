bb4f0c4785222dba16dae873aa230a650b0a9218
RVel37 <rvelissaris@gmail.com>
2023-04-28 00:17:48 +0100

    Update RSQLite2.R

--- app/data/RSQLite2.R
+++ app/data/RSQLite2.R
@@ -12,32 +12,25 @@ DBI::dbWriteTable(conn, "enrichr", en_df)
 DBI::dbWriteTable(conn, "contrasts",cont_df)
 DBI::dbWriteTable(conn, "metadata", met_df)
 
-gse_list <- c("GSE112057", "GSE123141", "GSE83687")
-
-#----------- test variables (to be deleted)
-normalization <- "CPM"
-gene_name <- "MT-RNR1"
 
 #-------------------------------------------------
-get_exp_plot_data <- function(gene_name, normalization) {
 # EXPRESSION PLOT
+#-------------------------------------------------
+
+get_exp_data <- function(gene_name, normalization) {
 conn <- DBI::dbConnect(RSQLite::SQLite(), "data.sqlite") #open conn
 
 # use ens2sym to match inputted gene with ENSEMBL ID
 ens2sym <- dplyr::tbl(conn, "ens2sym")
-
 ens_id <- ens2sym %>%
   filter(gene_name == !!gene_name) %>%
   select(gene_id) %>%
   pull()
 
-###### find correct GSE + correct normalization
-
+###### find correct study + normalization
 #filter tables for normalization
-suffix <- switch(normalization,
-                 CPM = "_cpm",
-                 TPM = "_tpm",
-                 RPKM = "_rpkm")
+suffix <- switch(normalization,CPM="_cpm",TPM="_tpm",RPKM="_rpkm")
+gse_list <- c("GSE112057", "GSE123141", "GSE83687")
 
 for (gse in gse_list) {
   expTable <- dplyr::tbl(conn, paste0(gse, suffix)) %>% collect() #loop through GSEs
@@ -50,12 +43,57 @@ for (gse in gse_list) {
 # add conditions (from metadata table)
 metadata <- dplyr::tbl(conn,"metadata") %>% collect
 
-longExpDf <- expDf %>% pivot_longer(cols = -gene_id, names_to="sample_id", values_to="expression") %>%
+longExpDf <- expDf %>%
+  pivot_longer(cols = -gene_id, names_to="sample_id", values_to="expression") %>%
   collect()
 
 longExpDf <- left_join(longExpDf,metadata, by = "sample_id")
 
 return(expDf)
+DBI::dbDisconnect(conn) #close conn
+}
+
+
+#------------- test variable
+contrast <- "Crohn vs. Control"
+
+#-------------------------------------------------
+# VOLCANO PLOT
+#-------------------------------------------------
+
+get_volcano_data <- function(gene, contrast) {
+conn <- DBI::dbConnect(RSQLite::SQLite(), "data.sqlite")
+
+####### find correct study (GSE)
+gse_list <- c("GSE112057", "GSE123141", "GSE83687")
+
+degStudy <- dplyr::tbl(conn, paste0(study, "_degs")) %>% collect() # DEGs for selected study
+
+
+######### Find selected gene (highlighted on volcano plot)
+ens2sym <- dplyr::tbl(conn, "ens2sym") # extract ENSEMBL ID
+ens_id <- ens2sym %>%
+  filter(gene_name == !!gene_name) %>%
+  select(gene_id) %>%
+  pull()
+
+selected_gene <- dplyr::tbl(conn, paste0(gse, "_degs")) %>%
+  filter(gene_id == ens_id) %>%
+  collect()
+
+
+####### get contrasts
+pair <- strsplit(contrast, " vs. ")[[1]]
+
+contrasts <- degStudy %>%
+  filter(numerator == pair[[1]] & denominator == pair[[2]])
+
+###### combine into list (main data frame + highlighted gene)
+
+vol_list <- list(contrasts, selected_gene)
+
+return(vol_list)
 DBI::dbDisconnect(conn)
 }
+
 
