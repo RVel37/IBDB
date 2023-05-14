1297e286be65264aa5861dc764a55e7982b1db0f
jmsdao <jmsdao@gmail.com>
2023-05-14 01:56:26 -0500

    Remove files, small edits

--- app/view/c_download.R
+++ app/view/c_download.R
@@ -15,9 +15,8 @@ ui <- function(id) {
   tabPanel(
     title = "Download",
     id = "download-tab",
-    DownloadPageContents(),
-    hr(),
-    DT$dataTableOutput(ns('downloadLinks'))
+    # DownloadPageContents(),
+    includeMarkdown("app/static/downloads.md"),
   )
 }
 
@@ -29,31 +28,5 @@ ui <- function(id) {
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-
-    output$downloadLinks <- DT$renderDT({
-      tibble(
-        File = c(
-          "metadata.csv",
-          "contrasts.csv",
-          "GSE126848_degs.csv.gz",
-          "GSE135251_degs.csv.gz",
-          "GSE126848_gene_exp.csv.gz",
-          "GSE135251_gene_exp.csv.gz",
-          "enrichr_res.csv.gz"
-        )
-      ) %>%
-        mutate(
-          Download = paste0(
-            "<a href='",
-            paste0("https://ibdb-data.s3.amazonaws.com/", File),
-            "' target='_blank'>link</a>"
-          )
-        ) %>%
-        DT$datatable(
-          selection = list(mode = "none"),
-          rownames = FALSE, escape = FALSE, options = list(dom = 't')
-        )
-    })
-
   })
 }
